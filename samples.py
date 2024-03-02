9f43e3b21c338c5a6addede6e7174bca42993461
Aleyna Akyuz <aleyna@Aleyna-MacBook-Pro-5.local>
2024-03-02 17:37:25 -0500

    changing z_max in samples.py

--- samples.py
+++ samples.py
@@ -5,7 +5,7 @@ import pycbc.workflow.configuration as wfc
 import h5py
 
 rho = 2*10**(-7)
-z_max = 100
+z_max = 7000
 time = 5
 file_name = 'parameters.h5'
 path = ['/Users/aleyna/Pycbc/confusion-noise-3g-master-2/dataset_sim/population_files/prior_files/o1o2o3_lvk_bns_upper_5Hz.ini']
@@ -19,7 +19,9 @@ merger_rate_dens = merger_rate_density(sfr_madau_dickinson_2014, 'inverse', rho_
 coa_rate = coalescence_rate(merger_rate_dens, maxz=z_max, npoints=1000)
 dist = distance_from_rate(total_rate, coa_rate)
 
-samples = round(time * rho * dist**3)
+samples = round(time * rho * dist**3 * (4/3) * np.pi)
+
+print(samples)
 
 data = joint_dist.rvs(samples)
 
